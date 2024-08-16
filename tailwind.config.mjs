ec8e7b8079de0da6b04195b91cb2642b7c06af26
jose32coder <josemanuel180601@gmail.com>
2024-08-15 21:13:12 -0400

    Responsive full

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: '640px',
+      smCustom: '500px', // Agregamos un nuevo breakpoint personalizado
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
